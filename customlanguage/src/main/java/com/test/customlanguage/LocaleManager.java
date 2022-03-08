bd2e3ebd60ee57562f4ad89d2be8a4bc79f15b05
sushant chiplunkar <sushantchiplunkar.developer@gmail.com>
2022-03-08 09:47:33 +0530

    Initial commit

---
+++ customlanguage/src/main/java/com/test/customlanguage/LocaleManager.java
@@ -0,0 +1,35 @@
+package com.test.customlanguage;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.util.DisplayMetrics;
+
+import java.util.Locale;
+
+public class LocaleManager {
+
+
+    private static void updateResources(Context context, String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Resources res = context.getResources();
+        Configuration config = new Configuration(res.getConfiguration());
+        config.locale = locale;
+        res.updateConfiguration(config, res.getDisplayMetrics());
+    }
+
+    public static void setAppLocale(Context context,String localeCode){
+        Resources resources = context.getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        Configuration config = resources.getConfiguration();
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN_MR1){
+            config.setLocale(new Locale(localeCode.toLowerCase()));
+        } else {
+            config.locale = new Locale(localeCode.toLowerCase());
+        }
+        resources.updateConfiguration(config, dm);
+    }
+}
